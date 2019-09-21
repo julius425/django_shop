4c3af664570cac816e372f1e3ae48c7c54243c79
julius425 <juliusk425@gmail.com>
2019-09-21 19:26:58 +0300

    added partial security middleware

---
+++ django_shop/django_shop/middleware.py
@@ -0,0 +1,26 @@
+import re
+from django.conf import settings
+from django.shortcuts import HttpResponseRedirect
+
+
+class SecureRequiredMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        site_url = request.META['HTTP_HOST']
+        path = request.path_info
+
+        if not request.is_secure() and path in settings.SECURE_URLS:
+            redirect_url = "https://{}{}".format(site_url, path)
+            return HttpResponseRedirect(redirect_url)
+        elif request.is_secure() and path not in settings.SECURE_URLS:
+            redirect_url = "http://{}{}".format(site_url, path)
+            return HttpResponseRedirect(redirect_url)
+        else:
+            pass
